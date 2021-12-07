18ae31fe0fa906bc4b1b8bf2e6a96fd25f74e9c5
jgcruz81 <juangcruz81@gmail.com>
2021-12-06 21:09:27 -0500

    yes

--- src/com/amazon/ata/hashingset/partsmanager/DevicePart.java
+++ src/com/amazon/ata/hashingset/partsmanager/DevicePart.java
@@ -1,6 +1,7 @@
 package com.amazon.ata.hashingset.partsmanager;
 
 import java.util.List;
+import java.util.Objects;
 
 public class DevicePart {
     private String manufacturer;
@@ -41,5 +42,26 @@ public class DevicePart {
     public String toString() {
         return String.format("Device Part: {manufacturer: %s, manufacturersPartNumber: %s, devicesUsedIn: %s}",
                 manufacturer, manufacturersPartNumber, devicesUsedIn);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        //if this object being compared is the same as the object you're comparing to, they are equal
+        if (this == o) return true;
+
+        //if the object being compared is null or a different class than the object you're comparing to, they are not equal
+        if (o == null || getClass() != o.getClass()) return false;
+
+        //instatiate an object of the class to used in comparison
+        DevicePart that = (DevicePart) o;
+
+        //use the class data members to determine equality
+        //note the use of the string class .equals to check attributes
+        return getManufacturer().equals(that.getManufacturer()) && getManufacturersPartNumber().equals(that.getManufacturersPartNumber());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getManufacturer(), getManufacturersPartNumber());
     }
 }
